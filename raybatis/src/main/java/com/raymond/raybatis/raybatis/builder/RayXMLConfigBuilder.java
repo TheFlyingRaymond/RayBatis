f217a6ebc56ac151c9c097fab26c071913769713
suncheng11 <suncheng11@meituan.com>
2024-12-16 17:39:00 +0800

    RayEnvironmentBuilder

--- raybatis/src/main/java/com/raymond/raybatis/raybatis/builder/RayXMLConfigBuilder.java
+++ raybatis/src/main/java/com/raymond/raybatis/raybatis/builder/RayXMLConfigBuilder.java
@@ -114,54 +114,8 @@ public class RayXMLConfigBuilder {
         }
     }
 
-    private void parseEnvironments(XNode context) {
-        if (Objects.isNull(context)) {
-            throw new RayConfigParseException("未找到environments节点");
-        }
-
-        if (null == environment) {
-            environment = context.getStringAttribute("default");
-        }
-
-        for (XNode node : context.getChildren()) {
-            String id = node.getStringAttribute("id");
-            if (!targetEnvironment(id)) {
-                log.info("未采用的环境配置:{}", id);
-                continue;
-            }
-
-            RayEnvironment environment = new RayEnvironment();
-            environment.setDataSource(parseDataSources(node.evalNode("dataSource")));
-            configuration.setEnvironment(environment);
-        }
-    }
-
-    private DataSource parseDataSources(XNode context) {
-        if (null == context) {
-            throw new RayConfigParseException("未找有效的DataSource配置");
-        }
-        HikariConfig config = new HikariConfig();
-        Properties prop = context.getChildrenAsProperties();
-        config.setDriverClassName(prop.getProperty("driver"));
-        config.setJdbcUrl(prop.getProperty("url"));
-        config.setUsername(prop.getProperty("username"));
-        config.setPassword(prop.getProperty("password"));
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-        return new RaySimpleDataSource(config);
-    }
-
-    private boolean targetEnvironment(String id) {
-        if (null == environment || environment.length() == 0) {
-            throw new RayConfigParseException("未找到有效的默认环境ID");
-        }
-
-        if (null == id || id.length() == 0) {
-            throw new RayConfigParseException("环境配置项ID非法");
-        }
-
-        return environment.equals(id);
+    public void parseEnvironments(XNode context) {
+        new RayEnvironmentBuilder(configuration, context, environment).parse();
     }
 
     public RayBatisConfiguration getConfiguration() {
